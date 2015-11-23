bf6de5a080aa87d2b1dd30fbaca5973e1ef573ae
emilymeroni <emily.1989@hotmail.it>
2015-11-23 19:40:13 +0100

    Starting off with userOptions

--- src/js/UserOptions.js
+++ src/js/UserOptions.js
@@ -8,6 +8,11 @@ memoryCardGame.UserOptions = function(params){
 		CSS: {
 			USER_OPTIONS_WRAPPER: 'user-options-wrapper',
 			OPTION_PANEL_CLASS: 'user-options-panel'
+		},
+		TEXT: {
+			PICTURE_NUMBER: 'Number of pictures:',
+			USER_OPTION_PANEL: 'User option panel',
+			CLOSE: 'Close'
 		}
 	};
 
@@ -29,18 +34,33 @@ memoryCardGame.UserOptions = function(params){
 	var draw = function() {
 		//TODO: Create utils library for dom elements
 		var userOptionsPanel = $('<div></div>').addClass(CONST.CSS.OPTION_PANEL_CLASS);
-		var userOptionsTitle = $('<h2></h2>').text('User option panel');
-		var closeButton = $('<button></button>').text('Close');
+		drawHeader(userOptionsPanel);
+		drawOptionsForm(userOptionsPanel);
+		drawFooter(userOptionsPanel);
+		self.container.append(userOptionsPanel);
+		//TODO: Create a global class for memoryGame and append panel to it
+		$('body').append(self.container);
+	};
+
+	var drawHeader = function(rootNode) {
+		var userOptionsTitle = $('<h2></h2>').text(CONST.TEXT.USER_OPTION_PANEL);
+		rootNode.append(userOptionsTitle);
+	};
+
+	var drawOptionsForm = function(rootNode) {
+		var howManyPicturesLabel = $('<label></label>').text(CONST.TEXT.PICTURE_NUMBER);
+		var howManyPictures = $('<input type="text">');
+		howManyPicturesLabel.append(howManyPictures);
+		rootNode.append(howManyPicturesLabel);
+	};
+
+	var drawFooter = function(rootNode) {
+		var closeButton = $('<button></button>').text(CONST.TEXT.CLOSE);
 		closeButton.click(function(){
 			self.container.hide();
 			new memoryCardGame.GameManager();
 		});
-
-		userOptionsPanel.append(userOptionsTitle);
-		userOptionsPanel.append(closeButton);
-		self.container.append(userOptionsPanel);
-		//TODO: Create a global class for memoryGame and append pannel to it
-		$('body').append(self.container);
+		rootNode.append(closeButton);
 	};
 
 	init.call(this);
