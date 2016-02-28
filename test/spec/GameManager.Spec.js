bb24c800d2ab4d0db32e19f44d86c8e0d5886269
emilymeroni <emily.1989@hotmail.it>
2016-02-28 21:42:16 +0100

    Adding tests

--- test/spec/GameManager.Spec.js
+++ test/spec/GameManager.Spec.js
@@ -1,40 +1,80 @@
-describe('GameManager', function() {
+describe('GameManager', function () {
 
     'use strict';
 
     var CONST;
     var gameManager;
 
-    beforeEach(function() {
+    beforeEach(function () {
 
         CONST = {
             CSS: {
-                ROOT: 'memory-card-game',
-                CARDS_CLASS: 'memory-cards'
+                ROOT: 'memory-card-game'
             },
             SELECTOR: {
-                CARDS_CLASS: '.memory-cards'
+                ATTEMPTS_NUMBER: '.attempts-number',
+                DECK: '.memory-cards',
+                STATS: '.current-stats-container',
+                TIMER: '.timer',
+                GAME_CONTAINER: '.dummy'
             }
         };
 
         gameManager = new memoryCardGame.GameManager({
-            gameContainer: '.dummy'
+            gameContainer: CONST.SELECTOR.GAME_CONTAINER
         });
     });
 
-    it('is the class that I need to instantiate to play a new memory game', function() {
+    it('is the class that I need to instantiate to play a new memory game', function () {
         expect(gameManager).toBeDefined();
     });
 
-    describe('it\'s container property:', function() {
+    describe('it\'s container property:', function () {
 
-        it('is a jQuery object holding the HTML node that contains all the elements for you to play', function() {
+        it('is a jQuery object holding the HTML node that contains all the elements for you to play', function () {
             expect(gameManager.container).toExist();
         });
 
-       it('is associated to the "memory-board" CSS class', function() {
+        xit('is contained within a configurable container', function () {
+        });
+
+        it('is associated to the "memory-board" CSS class', function () {
             expect(gameManager.container).toHaveClass(CONST.CSS.ROOT);
         });
+
+        describe('contains:', function () {
+
+            it('an instance of a deck', function () {
+                expect(gameManager.container.find(CONST.SELECTOR.DECK).length).toEqual(1);
+            });
+
+            it('an instance of a statistics panel', function () {
+                expect(gameManager.container.find(CONST.SELECTOR.STATS).length).toEqual(1);
+            });
+
+            it('a timer', function () {
+                expect(gameManager.container.find(CONST.SELECTOR.TIMER).length).toEqual(1);
+            });
+        });
+    });
+
+    describe('.onHandFinishedHandler():', function () {
+
+        it('increases the attempts counter by one', function () {
+            gameManager.onHandFinishedHandler();
+            expect(parseInt(gameManager.container.find(CONST.SELECTOR.ATTEMPTS_NUMBER).text(), 10)).toEqual(1);
+        });
+    });
+
+    describe('.onHandInvalidHandler():', function () {
+
+        it('increases the attempts counter by one', function () {
+            gameManager.onHandFinishedHandler();
+            expect(parseInt(gameManager.container.find(CONST.SELECTOR.ATTEMPTS_NUMBER).text(), 10)).toEqual(1);
+        });
+    });
+
+    xdescribe('.onCardsAllFlippedHandler():', function () {
     });
 
 });
